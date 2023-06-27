2f13d2ed27eee13c7cecaffaf66ec3e66db7cac0
ma-boles <120141149+ma-boles@users.noreply.github.com>
2023-06-26 18:37:21 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+function toggle () {
+    var shareMenu = document.getElementById("shareMenu");
+    if (shareMenu.style.display === "none"){
+        shareMenu.style.display = "inline"
+    } else {
+        shareMenu.style.display = "none"
+    }
+}
